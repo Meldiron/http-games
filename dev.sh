7bca907551d96c3d71773798980f7acf2e0d7325
Matej Bačo <matejbaco2000@gmail.com>
2025-11-10 14:43:49 +0100

    Fix CICD

--- dev.sh
+++ dev.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-source .env
-
-if [ -z "$_APP_DATABASE_OVERRIDE" ]; then
+if [ -f ".env" ] && grep -q "^_APP_DATABASE_OVERRIDE=" .env; then
+    # Variable is present in .env file, do nothing
+    :
+else
+    # Variable not in .env file, set default value
     _APP_DATABASE_OVERRIDE=local_$(date +%Y%m%d%H%M%S)
     export _APP_DATABASE_OVERRIDE
 fi
