6826147e8c0469ce2581871a3f9e9d88aa72481b
Matej Bačo <matejbaco2000@gmail.com>
2025-11-11 18:43:10 +0100

    linter fix

--- docs/lib/layout.shared.tsx
+++ docs/lib/layout.shared.tsx
@@ -1,14 +1,21 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import Image from "next/image";
 
 export function baseOptions(): BaseLayoutProps {
   return {
     githubUrl: "https://github.com/meldiron/http-games",
     nav: {
-      title: <>
-        <img src="/logo.svg" alt="Logo" className="h-5 hidden dark:block" />
-        <img src="/logo-dark.svg" alt="Logo" className="h-5 block dark:hidden" />
-        HTTP Games
-      </>,
+      title: (
+        <>
+          <Image src="/logo.svg" alt="Logo" className="h-5 hidden dark:block" />
+          <Image
+            src="/logo-dark.svg"
+            alt="Logo"
+            className="h-5 block dark:hidden"
+          />
+          HTTP Games
+        </>
+      ),
     },
   };
 }
